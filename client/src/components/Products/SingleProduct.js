3b3e528fec1eb37a882b4b08e5a7ffc5074028d6
poonam-tru <59247001+poonam-tru@users.noreply.github.com>
2023-02-02 15:16:18 +0530

    add fix

--- client/src/components/Products/SingleProduct.js
+++ client/src/components/Products/SingleProduct.js
@@ -7,12 +7,12 @@ import { useParams } from "react-router-dom";
 import { useDispatch, useSelector } from "react-redux";
 import { getSingleProductsData } from "../../actions/products";
 import { Button } from "@mui/material";
-import { incrementAction,decrementAction } from "../../actions/counter";
-import './SingleProduct.css'
+import { incrementAction, decrementAction } from "../../actions/counter";
+import "./SingleProduct.css";
 import Header from "../../pages/Header";
 
 function SingleProduct(props) {
-  const [btn , setBtn] = React.useState(true)
+  const [btn, setBtn] = React.useState(true);
   const param = useParams();
   const product = useSelector((state) => state.singleProduct.product);
   console.log(product, "product");
@@ -22,82 +22,98 @@ function SingleProduct(props) {
     dispatch(getSingleProductsData(param.id));
   }, []);
 
-  const count = useSelector(state => state.counterReducer.count)
-  console.log(count,"count---")
+  const count = useSelector((state) => state.counterReducer.count);
+  console.log(count, "count---");
 
-  const handleInc = (evt) =>{
-    dispatch(incrementAction())
-  }
+  const handleInc = (evt) => {
+    dispatch(incrementAction());
+  };
 
-  const handleDec = (evt) =>{
-    dispatch(decrementAction())
-  }
+  const handleDec = (evt) => {
+    dispatch(decrementAction());
+  };
 
   return (
     <div>
-    <Header />
-    <div style={{backgroundColor:'rgb(246, 249, 252)'}}>
-      <Card
-        sx={{
-          display: "flex",
-          justifyContent: "center",
-          padding: "25px 0px",
-          alignItems: "center",
-          maxWidth: 600,
-          margin: "auto",
-          width: "100%",
-        }}
-      >
-        {/* <CardMedia
+      <Header />
+      <div style={{ backgroundColor: "rgb(246, 249, 252)" }}>
+        <Card
+          sx={{
+            display: "flex",
+            justifyContent: "center",
+            padding: "25px 0px",
+            alignItems: "center",
+            maxWidth: 600,
+            margin: "auto",
+            width: "100%",
+          }}
+        >
+          {/* <CardMedia
           component="img"
           sx={{ width: "100%" }}
           image={u.img}
           alt=""
         /> */}
 
-        <Box sx={{ display: "flex", flexDirection: "column" }}>
-          <CardContent
-            sx={{
-              flex: "1 0 auto",
-              padding: 0,
-              paddingBottom: "0px !important",
-            }}
-          >
-            <h2 style={{ margin: 0, paddingBottom: 25 }}>{product.name}</h2>
+          <Box sx={{ display: "flex", flexDirection: "column" }}>
+            <CardContent
+              sx={{
+                flex: "1 0 auto",
+                padding: 0,
+                paddingBottom: "0px !important",
+              }}
+            >
+              <h2 style={{ margin: 0, paddingBottom: 25 }}>{product.name}</h2>
 
-            <p style={{ margin: 0 }}>{product.description}</p>
-            <p style={{ margin: 0,paddingTop:20 }}><span style={{fontWeight:'bolder'}}>Price:</span> {product.price}</p>
-            <p style={{ margin: 0,paddingTop:20 }}><span style={{fontWeight:'bolder'}}>Quantity:</span> {product.quantity}</p>
+              <p style={{ margin: 0 }}>{product.description}</p>
+              <p style={{ margin: 0, paddingTop: 20 }}>
+                <span style={{ fontWeight: "bolder" }}>Price:</span>{" "}
+                {product.price}
+              </p>
+              <p style={{ margin: 0, paddingTop: 20 }}>
+                <span style={{ fontWeight: "bolder" }}>Quantity:</span>{" "}
+                {product.quantity}
+              </p>
 
-            <div>
-              <Button variant="contained" 
-              style={{marginTop:'20px'}} 
-              >
-              Add to Cart
-              </Button>
-            </div>
+              <div style={{ marginTop: "20px" }}>
+                <Button
+                  variant="contained"
+                  style={{ minWidth: "30px" }}
+                  onClick={handleDec}
+                  disabled={count === 0}
+                >
+                  -
+                </Button>
+                <span
+                  style={{
+                    width: 30,
+                    display: "inline-block",
+                    textAlign: "center",
+                  }}
+                >
+                  {count}
+                </span>
+                <Button
+                  variant="contained"
+                  style={{ minWidth: "30px" }}
+                  onClick={handleInc}
+                  disabled={product.quantity === count}
+                >
+                  +
+                </Button>
+              </div>
 
-            <div style={{marginTop:'20px'}}>
-              <Button variant="contained"
-              style={{minWidth:'50px',marginRight:'13px'}} 
-              onClick={handleInc}
-              disabled ={product.quantity === count }
-              >+</Button>
-              <span>{count}</span>
-              <Button variant="contained"
-              style={{minWidth:'50px',marginLeft:'13px'}}  
-              onClick={handleDec}
-              disabled ={count === 0 }
-              >-</Button>
-
-            </div>
-
-          </CardContent>
-        </Box>
-      </Card>
+              <div>
+                <Button variant="contained" style={{ marginTop: "20px" }}>
+                  Add to Cart
+                </Button>
+              </div>
+            </CardContent>
+          </Box>
+        </Card>
       </div>
     </div>
   );
 }
 
-export default SingleProduct
+export default SingleProduct;
